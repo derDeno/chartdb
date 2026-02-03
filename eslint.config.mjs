e4f7d8058114987b807ea8f3444e75a64d15ebca
Deniz Celebi <5246652+derDeno@users.noreply.github.com>
2026-02-03 18:47:55 +0100

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,6 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
-import prettier from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -84,5 +83,4 @@ export default [
             },
         },
     },
-    prettier,
 ];
